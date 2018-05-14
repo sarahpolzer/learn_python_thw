0853ccedbf79d9aaf37ed133a47ced40c09dad5b
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-05-14 16:01:28 -0400

    Complete lesson 11

---
+++ lesson_11.py
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm tabbed in." #this line is like pressing table because of \t
+persian_cat = "I'm split\non a line." #this line is splitting the string with \n
+backslash_cat = "I'm \\ a \\ cat." #this line is creating \ with \\ within the string
+fat_cat = """ #these are creating a string that splits into multiple lines
+I'll do a list:
+\t* Cat food 
+\t* Fishies
+\t* Catnip\n\t* Grass """ #\t* is creating little star bullet points
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+Sarah=5**2
+print(Sarah)
+
