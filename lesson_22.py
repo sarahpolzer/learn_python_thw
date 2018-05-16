d39113711d412cd37b99a39ec8b2a45298a88759
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-05-16 14:18:13 -0400

    lesson 22

---
+++ lesson_22.py
@@ -0,0 +1,35 @@
+#Exercise 22: What do You Know So Far
+#Review of symbols that I have learned 
+#print'word') -> prints out word string
+#print - # symbol makes comments and whatever comes after it 
+#does not run
+# + (plus)
+#- (minus)
+# / (division)
+# * (multiplication)
+# % (modulus)
+# < (less than)
+# > greater-than
+#<=less than equal
+# >= greater than or equal
+
+# = variable assignment
+# Can combine strings and variables into print with comma
+#can embed variables in strings with syntax f("words {varnam} words")
+# empty bracket with .format(variable) fills empty bracket with word in format
+#\n skips lines in a string
+#""" makes it so you can skip lines in a string """ 
+# input() prompts user
+#from sys import argv
+#argv-passes arguments from the command line
+#script, first, second, third=argv
+#so when typing in command line say python filename first second third
+# open(file-name): opens file
+#close-files and saves filei in editor
+#read-reads contents of file
+#readline-reads just one line of the textfile
+#truncate- empties the file. watch out if you care
+#write('stuff')--writes stuff to the file
+#seek(0)->move the read write function to beginning of file
+#def:function
+#
