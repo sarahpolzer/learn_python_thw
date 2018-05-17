992b514534fbc1e0fe8dc44eb0832e0238162c54
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-05-17 13:33:13 -0400

    lesson 34

---
+++ lesson_34.py
@@ -0,0 +1,9 @@
+#animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+#1) The animal at 1 is python3.6
+#2) The third animal is at 2 and is a peacock
+#3) The first animal is at 0 and is a bear
+#4) The animal at 3 is a kangaroo
+#5) The fifth animal is at 4 and is a whale
+#6) The animal at 2 is 'python3.6'
+#7) The sixth animal is at five and is a platypus
+#8) The animal at 4 is a kangaroo 
