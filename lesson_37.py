2ca0d9ce0045673b30fc819bfb2b1d878f43fac2
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-05-21 10:24:42 -0400

    lesson 37

---
+++ lesson_37.py
@@ -0,0 +1,102 @@
+#keywords
+#and: Logical and  True and False == False
+#as: part of a with-as statement
+#assert : Assert ensure that something is true
+#break: Stop this loop right now
+#class : Define a class Person(object)
+#continue: Don't process more of the loop, do it again
+#def: Define a function
+#del: delete from dictionary
+#elif: elseif condition
+#else: else condition
+#except: if an exception happens. do this
+#exec: run a string as python
+#finally: Exceptions or not, finally do this no matter what
+#for: Loop over a collection of things
+#from: Importing specific parts of a module
+#gloal: Declare that you want a global variable
+#if : if condition
+#import: import a module into this one to use
+#in: Part of for-loops. Also a test of X in Y
+#is: like== to test equality
+#lambda: Create a short anonymous function
+#not: logical not
+#or: logical or
+#pass: This block is empty
+#print: print this string
+#raise: raise an exception when things go wrong
+#return: exit the function with a return value
+#try: Try this block, and if exception, go to except
+#while: while loop
+#with: with an exception as a variable do.
+#yield: pause here and return to caller
+
+##Data Types
+#True: True boolean value
+#False: False boolean value
+#None; Represents "nothing" or "no value"
+#bytes: Stores bytes, maybe of text, PNG, file, etc
+#strings: Stores textual information
+#numbers: stores integers
+#floats: stores decimals
+#lists: Stores a list of things
+#dicts: Stores a key=value mapping of things
+
+##Escape Sequences
+#\\ Backslash
+#\' Single-quote
+# \" Double-quote
+#\a Bell
+#\b Backspace
+# \f Formfeed
+# \n Newline
+#\r Carriage
+# \t Tab
+# \v Vertical tab
+##Old Style String Formats
+#%d Decimal integers
+#%i same as %d
+#%o octal number
+#%u Unsigned decimal
+# %x Hexadecimal lowercase
+# %X Hexadecimal uppercase
+# %e Exponential notation, lowercase 'e'
+# %E Exponential notation, uppercase 'E'
+#%f Floating point real number
+# %F same as %f
+#%g Either %f or %e, whichever is shorter
+#%G Same as %g
+#%c Character Format
+#%r Repr format (debugging format)
+#%s String format
+#%% A percent sign
+# + Addition
+# - Subtraction
+# * Multiplication
+# ** Power of
+# / Division
+# // Floor division 
+# % mod
+# < less than 
+# > Greater than 
+# <= less than equal
+# >= Greater than equal
+# == Equal
+# != not equal
+# () parenthesis
+# [] list brackets
+#Dict curly braces {}
+# @ At(decorators)
+# , comma
+# : colon
+# . Dot
+# = Assign equal
+# ; semi-colon
+# += add and assign
+#-= subtract and assign
+# *= multiply and assign
+# /= divide and assign
+# //= Floor divide and assign
+# %= modulus assign
+# **= power assign
+
